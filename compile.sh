7ecae8d97ad6ef052f754aa4ef25f59263620334
Paul Lorenz <plorenz@gmail.com>
2008-05-19 21:12:32 +0000

    make sure all test workflows load to db fix compile script Date: 2008-04-11 10:30:50

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm -r out/*
 
-ghc -Wall -fglasgow-exts --make -isrc -o bin/wf-xml-console -hidir out -odir out src/ConsoleXmlMain.hs `find src -name "*.lhs"`
+ghc -Wall -fglasgow-exts --make -isrc -o bin/wf-xml-console -hidir out -odir out src/ConsoleXmlMain.hs
 
-ghc -Wall -fglasgow-exts --make -isrc -o bin/wf-db-console -hidir out -odir out src/ConsoleDbMain.hs `find src -name "*.lhs"`
+ghc -Wall -fglasgow-exts --make -isrc -o bin/wf-db-console -hidir out -odir out src/ConsoleDbMain.hs
 
-ghc -Wall -fglasgow-exts --make -isrc -o bin/wf-loader -hidir out -odir out src/LoaderMain.hs `find src -name "*.lhs"`
+ghc -Wall -fglasgow-exts --make -isrc -o bin/wf-loader -hidir out -odir out src/LoaderMain.hs
